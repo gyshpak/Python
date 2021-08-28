f69c4ce792309a59ce1d4706114d6e4c8aafbf8a
Olga Shpak <oashpak@gmail.com>
2021-08-28 13:44:49 +0300

    Added Task1_2.py

---
+++ Task1_2.py
@@ -0,0 +1,8 @@
+value_a = int(input("Please input a: "))
+value_b = int(input("Please input b: "))
+
+print("a + b =", value_a+value_b)
+print("a - b =", value_a-value_b)
+print("a * b =", value_a*value_b)
+print("a / b =", value_a/value_b)
+print("a ** b =", value_a**value_b)
